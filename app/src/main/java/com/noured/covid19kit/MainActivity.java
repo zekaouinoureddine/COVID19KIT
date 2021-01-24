f47b4cda8be31d2b3fa3e381b4e2c5ba23b36db1
Nour Eddine ZEKAOUI <noureddinezekaoui@gmail.com>
2021-01-24 22:59:48 +0100

    🧰 Bot Integration ️🎰

--- app/src/main/java/com/noured/covid19kit/MainActivity.java
+++ app/src/main/java/com/noured/covid19kit/MainActivity.java
@@ -6,11 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mButtonContactUs;
     private Button mButtonStart;
+    private ImageView mButtonBot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +21,9 @@ public class MainActivity extends AppCompatActivity {
 
         mButtonContactUs = (Button) findViewById(R.id.main_activity_contact_us);
         mButtonStart = (Button) findViewById(R.id.main_activity_start);
+        mButtonBot = (ImageView) findViewById(R.id.main_activity_botbtn);
 
+        // Contact Button Listener
         mButtonContactUs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,12 +34,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Start Button Listener
         mButtonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 // User Click on the Plant Button
                 Intent transitionActivityIntent = new Intent(MainActivity.this, TransitionActivity.class);
+                startActivity(transitionActivityIntent);
+            }
+        });
+
+        // Bot Button Listener
+        mButtonBot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // User Click on the Plant Button
+                Intent transitionActivityIntent = new Intent(MainActivity.this, BotActivity.class);
                 startActivity(transitionActivityIntent);
             }
         });
